6247c6ae67e0bddbd7a003e69e872565befb8310
Max Wang <mxawng@gmail.com>
2013-05-08 16:17:48 -0400

    Rewriting tree.rb to produce better head movement arrows

--- tree.rb
+++ tree.rb
@@ -14,63 +14,142 @@ OUTDIR = 'out'
 TEXFILE = 'trees.tex'
 PDFFILE = 'trees.pdf'
 
-#
-# qtree_convert - Convert a SWI-prolog atom or metachar into a Qtree token.
-#
-# Node labels are tokenized with their open-paren.  Inflectional heads are
-# tokenized with their full contents.
-#
-def qtree_convert(token, labels)
-  token.gsub!('_', "$'$")
 
-  # Lexical categories.
-  if token.end_with? '('
-    tok = token.chop
+###############################################################################
+#
+#  X-bar theory syntax tree node.
+#
 
-    # Simplified category.
-    if token.start_with? '*'
-      tok = tok[1..-1] + ' \edge[roof];'
+class XBarNode
+  attr_accessor :parent
+  attr_reader :type, :children, :token
+
+  def initialize(tok)
+    @type = :leaf
+    @parent = nil
+    @children = []
+    @label = nil
+    @movtype = nil
+
+    # Lexical categories are nodes; words are leaves.
+    if tok.end_with? '('
+      @type = :node
+
+      # Whether or not to use a roof.
+      if tok.start_with? '*'
+        tok = tok[1..-1]
+        @roof = true
+      end
+
+      tok = tok.chop.capitalize.gsub('p', 'P')
     end
 
-    ' [.' + tok.capitalize.gsub('p', 'P')
+    # Head movement source and destination.
+    if tok.include? '/'
+      tok, label = tok.split('/')
 
-  # Special-case tense.
-  elsif token.start_with? 'i('
-    ' [.I {[' + token[2...-1] + ']} ]'
+      if label.to_i.zero?
+        tok, label = label, tok
+        @movtype = :dst
+      else
+        @movtype = :src
+      end
 
-  # End-of-X.
-  elsif token == ')'
-    ' ]'
-
-  # Possessive head.
-  elsif token == 's'
-    ' {\'s}'
-
-  # Head movement (i.e., labeled).
-  elsif token.include? '/'
-    # Token with a label means head movement.
-    tok, label = token.split('/')
-
-    # Determine which end of the arrow this node is at.
-    if label.to_i.zero?
-      tok, label = label, tok
-      type = 'dst'
-    else
-      type = 'src'
+      @label = label
     end
 
-    # Italicize traces and light verbs.
-    tok = "$#{tok}$" if tok.size == 1
+    # Set the final token.
+    @token = tok
+  end
 
-    # Give the node an alphabetic label.
-    labels[label] ||= labels[:label].succ!.dup
+  def node?; @type == :node; end
+  def leaf?; @type == :leaf; end
 
-    # Output a qtree node.
-    " \\node(#{labels[label]} #{type}){#{tok}};"
-  else
-    ' {' + token + '}'
+  def add_child(child)
+    @children << child
+    child.parent = self
+  end
+
+  #
+  # Output the subtree in Qtree format.
+  #
+  def to_s
+    s, ddata = stringify(0, '`', {})
+
+    s = '\Tree' + s
+
+    # Draw head movement arrows.
+    ddata.each_value.inject(s) do |s, d|
+      # Compute arrow offset based on depths.
+      sdep = (2 * (4 + d[:depth] - d[:src])).to_i
+      ddep = (1.5 * (2 + d[:depth] - d[:dst])).to_i
+
+      l = d[:label]
+      sctl = "+(south:#{sdep})"
+      dctl = "+(south:#{ddep})"
+
+      s << "\n\\draw[semithick,->] "
+      s << "(#{l} src)..controls #{sctl} and #{dctl}..(#{l} dst);"
+    end
+  end
+
+  #
+  # Stringify the subtree as a Qtree, but also return depth-related metadata
+  # for optimizing head movement.
+  #
+  def stringify(depth, lstr, ddata)
+    # Replace underscores with '.
+    s = @token.gsub('_', "$'$")
+
+    # Stringify leaves.
+    if @type == :leaf
+      # Italicize traces, light verbs, and inflectional heads.
+      if @token == 'v' or @token == 't'
+        s = "$#{s}$"
+      elsif @parent.token == 'I'
+        s = "[#{s}]"
+      end
+
+      if not @label.nil?
+        # Head movement label accounting.
+        if not ddata.key? @label
+          ddata[@label] = { label: lstr.succ!.dup, depth: depth, }
+        end
+        ddata[@label][@movtype] = depth
+        ddata[@label][:depth] = [ddata[@label][:depth], depth].max
+
+        # Output a TikZ node.
+        s = " \\node(#{ddata[@label][:label]} #{@movtype}){#{s}};"
+      else
+        s = " {#{s}}"
+      end
+
+    # Stringify nodes.
+    elsif @type == :node
+      # Append roof string.
+      s += ' \edge[roof];' if @roof
+
+      # Stringify children.
+      cs = @children.map { |c| c.stringify(depth + 1, lstr, ddata).first }
+
+      s = " [.#{s} #{cs.join(' ')} ]"
+    end
+
+    # Update high water mark depth for unmatched labels.
+    ddata.each_value do |d|
+      d[:depth] = [d[:depth], depth].max unless d.key? :src and d.key? :dst
+    end
+
+    # Return string and depth data.
+    return s, ddata
   end
 end
+
+
+###############################################################################
+#
+#  Parse.
+#
 
 # Shift the Prolog file.
 prolog = ARGV.shift
@@ -100,25 +179,28 @@ trees = IO.popen(swipl, :err => [:child, :out]) do |io|
     s.gsub!(/dp\(d_\(d\(([\w\/. ]+)\),\s*np\(n_\(n\(([\w\/. ]+)\)\)\)\)\)/, '*dp(\1 \2)')
 
     # Regexp for tokenizing the tree.
-    re = /i\(\w+\)|[\w*]+\(|\)|[\w\/. ]+/
+    re = /[\w*]+\(|\)|[\w\/. ]+/
 
-    # Hash of head movement source/dest labels.
-    labels = {label: '`'}
+    # X-bar-ificate the tree output.
+    tree = s.scan(re).inject(XBarNode.new('')) do |node, token|
+      if token == ')'
+        node.parent
+      else
+        child = XBarNode.new(token)
+        node.add_child child
+        if child.node? then child else node end
+      end
+    end.children.first
 
-    # Q-tree-ificate the tree outputs.
-    qtree = s.scan(re).inject('\Tree') do |qtree, token|
-      qtree << qtree_convert(token, labels)
-    end
-
-    labels.delete :label
-    labels.each do |_, l|
-      qtree << "\n\\draw[semithick,->] "
-      qtree << "(#{l} src)..controls +(south:6) and +(south:11)..(#{l} dst);"
-    end
-
-    qtree
+    tree
   end
 end
+
+
+###############################################################################
+#
+#  Compile.
+#
 
 # LaTeX header/footer.
 PREAMBLE = %w{
@@ -134,9 +216,9 @@ EPILOGUE = [''] + %w{
 }
 
 # Wrap trees.
-trees.map! do |qtree|
+trees.map! do |tree|
   [ '\begin{tikzpicture}',
-    qtree,
+    tree,
     '\end{tikzpicture}',
   ]
 end
